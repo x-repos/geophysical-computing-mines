1dbab285d1e6fc5b94f60f359948ec252044318e
ben <sipkaro@gmail.com>
2023-09-29 04:22:05 -0600

    Auto Commit

--- assign3/c.c
+++ assign3/c.c
@@ -1,3 +1,6 @@
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -6,7 +9,15 @@
 // Allocation
 //////////////////////////////////////////////////////////////////
 float* allocate1DArray(int n){
+    /*
+    float* a = allocate1DArray(n);
+    */
     float* array = (float*)malloc(n * sizeof(float));
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = 0.0;
+    }
+    
     return array;
 }
 
@@ -32,6 +43,15 @@ float** allocate2DArray(int rows, int cols) {
     return matrix;
 }
 
+
+void free2DArray(float** array, int rows) {
+    for (int i = 0; i < rows; i++) {
+        free(array[i]);
+    }
+    free(array);
+}
+
+
 // Nothing
 //////////////////////////////////////////////////////////////////
 void print1DMatrix(float *a, int n){
@@ -49,6 +69,7 @@ void print2DMatrix(float **a, int n){
         printf("\n"); 
     }
 }
+
 
 // Solve the system of equations
 //////////////////////////////////////////////////////////////////
@@ -98,7 +119,7 @@ void backwardSubstitution(float** u, float* y, float* x, int n) {
     }
 }
 
-void linalgsolve(float** a, float* b, int n){
+float* linalgsolve(float** a, float* b, int n){
     float** l = allocate2DArray(n,n);
     float** u = allocate2DArray(n,n);
     float*  x = (float*)malloc(n * sizeof(float));
@@ -107,16 +128,230 @@ void linalgsolve(float** a, float* b, int n){
     luDecomp(a, l, u, n);
     forwardSubstitution(l, b, y, n);
     backwardSubstitution(u, y, x, n);
-    print1DMatrix(x,n);
+    free2DArray(l,n);
+    free2DArray(u,n);
+    return x;
 }
+
+
+// Linspace
+
+void linspace(float start, float end, int num, float* result) {
+    float delta = (end - start) / (num - 1);
+    for (int i = 0; i < num; i++) {
+        result[i] = start + delta * i;
+    }
+}
+
+// save matrix
+//////////////////////////////////////////////////////////////////
+void saveMatrixToFile(float **matrix, int rows, int cols, const char *filename, const char *mode) {
+    FILE *file = fopen(filename, mode);
+    if (file == NULL) {
+        printf("Error opening file!\n");
+        return;
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            fprintf(file, "%lf ", matrix[i][j]); // write each element with a space in between
+        }
+        fprintf(file, "\n"); // new line for each row
+    }
+
+    fclose(file);
+}
+
+void saveArrayToFile(float *data, int n, const char *filename) {
+    FILE *fp = fopen(filename, "w"); // Open the file for writing
+    if (fp == NULL) {
+        printf("Error opening file\n");
+        return; // Exit if file can't be opened
+    }
+
+    for (int i = 0; i < n; i++) {
+        fprintf(fp, "%lf\n", data[i]); // Write each element to the file, each on a new line
+    }
+
+    fclose(fp); // Close the file
+}
+
+
+// Internal function
+void Setup_Tridiagonal_Heterogeneous(float** A, float* a, float* b, float* c, int n) {
+    int i;
+    for (i = 0; i < n; i++) {
+        A[i][i] = b[i];
+    }
+    for (i = 1; i < n; i++) {
+        A[i][i - 1] = a[i - 1];
+    }
+    for (i = 0; i < n - 1; i++) {
+        A[i][i + 1] = c[i];
+    }
+    A[0][0] = 1.0;
+    A[0][1] = 0.0;
+    A[n - 1][n - 1] = 1.0;
+    A[n - 1][n - 2] = 0.0;
+}
+
+void ADI_Solution_Heterogeneous(float **U, float** K, float** F, float h, int n){
+    int i, j, ii, jj; 
+    int nx = n, ny = n;
+    float** U1 = allocate2DArray(n,n);
+    for (i = 0; i < nx; i++) {
+        U1[i][0] = U[i][0];
+    }
+    for (j = 0; j < ny; j++) {
+        U1[0][j] = U[0][j];
+    }
+    for (i = 0; i < nx; i++) {
+        U1[i][ny-1] = U[i][ny-1];
+    }
+    for (j = 0; j < ny; j++) {
+        U1[nx-1][j] = U[nx-1][j];
+    }
+
+    float *axmat, *bxmat, *cxmat, *bx, *ktemp;
+    float *aymat, *bymat, *cymat, *by; 
+    axmat = allocate1DArray(nx-1);
+    bxmat = allocate1DArray(nx);
+    cxmat = allocate1DArray(nx-1);
+    bx    = allocate1DArray(nx);
+    aymat = allocate1DArray(ny-1);
+    bymat = allocate1DArray(ny);
+    cymat = allocate1DArray(ny-1);
+    by    = allocate1DArray(ny);
+    ktemp = allocate1DArray(nx);
+
+    for (jj = 1; jj < ny-1; jj++) {
+        for (i = 0; i < nx-1; i++) {
+            axmat[i] = K[i][jj];
+        }
+        for (int i = 1; i < nx; i++)
+        {
+            ktemp[i] = K[i-1][jj];
+        }
+        for (i = 0; i < nx; i++) {
+            bxmat[i] = -2.0 * K[i][jj] - K[i][jj-1]-ktemp[i];
+        }
+        for (i = 1; i < nx; i++) {
+            cxmat[i-1] = K[i][jj];
+        }
+        float** Ax = allocate2DArray(n,n);
+        Setup_Tridiagonal_Heterogeneous(Ax, axmat, bxmat, cxmat, nx);
+
+        for (i = 0; i < nx; i++) {
+            bx[i] = -h * h * F[i][jj] - K[i][jj] * U[i][jj+1] - K[i][jj-1] * U[i][jj-1];
+        }
+        bx[0] = U[0][jj];
+        bx[nx-1] = U[nx-1][jj];
+
+        float *result = linalgsolve(Ax, bx, nx);
+        for (i = 0; i < nx; i++) {
+            U1[i][jj] = result[i];
+        }
+        free(result);
+        for (int i = 0; i < nx; i++) {
+            free(Ax[i]);
+        }
+        free(Ax);        
+    }
+
+    for (ii = 1; ii < nx-1; ii++) {
+
+        for (j = 0; j < ny-1; j++) {
+            aymat[j] = K[ii][j];
+        }
+        for (int j = 1; j < ny; j++)
+        {
+            ktemp[j] = K[ii][j-1];
+        }
+        for (j = 0; j < ny; j++) {
+            bymat[j] = -2.0 * K[ii][j] - K[ii-1][j] - ktemp[j];
+        }
+        for (j = 1; j < ny; j++) {
+            cymat[j-1] = K[ii][j];
+        }
+        float** Ay = allocate2DArray(n,n);
+        Setup_Tridiagonal_Heterogeneous(Ay, aymat, bymat, cymat, ny);
+        for (j = 0; j < ny; j++) {
+            by[j] = -h * h * F[ii][j] - K[ii][j] * U1[ii+1][j] - K[ii-1][j] * U1[ii-1][j];
+        }
+        by[0] = U1[ii][0];
+        by[ny-1] = U1[ii][ny-1];
+        float *result = linalgsolve(Ay, by, ny);
+        for (j = 0; j < ny; j++) {
+            U[ii][j] = result[j];
+        }
+
+        free(result);
+        for (int i = 0; i < nx; i++) {
+            free(Ay[i]);
+        }
+        free(Ay);
+       
+    }
+    for (int i = 0; i < nx; i++) {
+        free(U1[i]);
+    }
+    free(U1);
+}
+
 
 // Main
 //////////////////////////////////////////////////////////////////
-
-
-
-
-
 int main(){
-    int n = 100;
+    float t1 = -1.0, t2 = 1.0, a = 1, b = 1;
+    float xmin = 0.0, ymin = 0.0, xmax, ymax;
+    xmax = a, ymax = b;
+    int n = 100, nt = 500;
+    float* xx = allocate1DArray(n);
+    float* yy = allocate1DArray(n);
+    linspace(xmin, xmax, n, xx);
+    linspace(ymin, ymax, n, yy);
+    float h = xx[1] - xx[0];
+    float** u0 = allocate2DArray(n,n);
+
+    for (int j = 0; j < n; j++) {
+        u0[0][j] = t1;
+    }
+    for (int j = 0; j < n; j++) {
+        u0[n-1][j] = 0.0;
+    }
+    for (int i = 0; i < n; i++) {
+        u0[i][0] = 0.0;
+    }
+    for (int i = 0; i < n; i++) {
+        u0[i][n-1] = t2;
+    }
+    u0[0][0] = 0.0;
+    u0[0][n-1] = 0.0;
+    u0[n-1][0] = 0.0;
+    u0[n-1][n-1] = 0.0;
+
+    float** K = allocate2DArray(n,n);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            K[i][j] = 1.0 - 0.75 * exp(- (xx[j] - a/2.0) * (xx[j] - a/2.0) - (yy[i] - b/2.0) * (yy[i] - b/2.0));
+        }
+    }
+    float sourcesize = 0.1;
+    int halfsize = (int) round(sourcesize / a / h / 2.0);
+
+    float** ff = allocate2DArray(n,n);
+
+    for (int i = (n/2) - 1 - halfsize; i < (n/2) - 1 + halfsize; i++) {
+        for (int j = (n/2) - 1 - halfsize; j < (n/2) - 1 + halfsize; j++) {
+            ff[i][j] = 200.0 * t2;
+        }
+    }
+
+    for (int i = 0; i < nt; i++)
+    {
+        ADI_Solution_Heterogeneous(u0,K,ff,h,n);
+        saveMatrixToFile(u0,n,n,"results.txt","a");
+    }
+    free2DArray(K,n);
+    free2DArray(ff,n);
 }
---
+++ assign3/run.sh
@@ -0,0 +1,2 @@
+gcc c.c -lm
+a.out
